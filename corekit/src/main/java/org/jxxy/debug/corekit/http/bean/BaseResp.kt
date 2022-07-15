ac434c88ad52d979791f6152cdc71462160f6067
WangJiafeng <945102268@qq.com>
2022-07-15 23:32:39 +0800

    多类型recyclerAdapter——完成,多类型gson解析待完善

--- corekit/src/main/java/org/jxxy/debug/corekit/http/bean/BaseResp.kt
+++ corekit/src/main/java/org/jxxy/debug/corekit/http/bean/BaseResp.kt
@@ -1,7 +1,7 @@
 package org.jxxy.debug.corekit.http.bean
 
-class BaseResp<T> {
-    val code: Int? = null
-    var message: String? = null
-    var data: T? = null
-}
+class BaseResp<T>(
+    val code: Int? = null,
+    val message: String? = null,
+    val data: T? = null,
+)
