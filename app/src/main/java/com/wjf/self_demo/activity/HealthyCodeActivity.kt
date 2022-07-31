5f6d853df4cb20ce659e74ca1f4a7898f9bcafda
WangJiafeng <945102268@qq.com>
2022-07-31 17:11:59 +0800

    健康码优化

--- app/src/main/java/com/wjf/self_demo/activity/HealthyCodeActivity.kt
+++ app/src/main/java/com/wjf/self_demo/activity/HealthyCodeActivity.kt
@@ -29,8 +29,16 @@ class HealthyCodeActivity : BaseActivity<ActivityHealthyCodeBinding>() {
         view.timeDownTv.text = sbs2
 
         view.submitBtn.singleClick {
-            view.locationTopTv.text = view.inputTop.text
-            view.locationBottomTv.text = view.inputBottom.text
+            if (view.inputTop.text.toString().isNullOrEmpty()) {
+                view.locationTopTv.text = "西郊九韵城71"
+            } else {
+                view.locationTopTv.text = view.inputTop.text
+            }
+            if (view.inputBottom.text.toString().isNullOrEmpty()) {
+                view.locationBottomTv.text = "上海市/闵行区/华漕镇/繁兴路300弄"
+            } else {
+                view.locationBottomTv.text = view.inputBottom.text
+            }
             view.inputCard.visibility = View.GONE
         }
         view.healthyCodeImg.singleClick {
@@ -41,7 +49,7 @@ class HealthyCodeActivity : BaseActivity<ActivityHealthyCodeBinding>() {
     override fun setLayout(): Int = R.layout.activity_healthy_code
 
     override fun initData() {
-        ScheduleTask(lifecycle, 3, TimeUnit.MINUTES) {
+        ScheduleTask(lifecycle, 1, TimeUnit.SECONDS) {
             val curDate = Date(System.currentTimeMillis())
             val sbs2 = SpannableString(formatter.format(curDate))
             sbs2.setSpan(RelativeSizeSpan(1.3f), 17, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
