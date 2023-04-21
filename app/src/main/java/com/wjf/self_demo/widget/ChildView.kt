71c8507320a8f0f719300b0f1f3cccab44a75963
王佳峰 <wangjf6@yonghui.cn>
2023-04-21 18:49:50 +0800

    事件分发日志优化

--- app/src/main/java/com/wjf/self_demo/widget/ChildView.kt
+++ app/src/main/java/com/wjf/self_demo/widget/ChildView.kt
@@ -2,9 +2,9 @@ package com.wjf.self_demo.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import com.orhanobut.logger.Logger
 
 class ChildView @JvmOverloads constructor(
     context: Context,
@@ -16,27 +16,47 @@ class ChildView @JvmOverloads constructor(
         setOnClickListener(this)
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val result = super.onTouchEvent(event)
-//        Logger.d("ChildView onTouchEvent:$result")
-        when (event?.action) {
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        when (ev?.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                Logger.d("ChildView ACTION_DOWN")
+                Log.d("wjftc", "ChildView dispatchTouchEvent ACTION_DOWN")
             }
             MotionEvent.ACTION_MOVE -> {
-//                Logger.d("ParentLayout ACTION_MOVE")
+                Log.i("wjftc", "ChildView dispatchTouchEvent ACTION_MOVE")
+                return true
             }
             MotionEvent.ACTION_UP -> {
-                Logger.d("ChildView ACTION_UP")
+                Log.w("wjftc", "ChildView dispatchTouchEvent ACTION_UP")
             }
             MotionEvent.ACTION_CANCEL -> {
-                Logger.d("ChildView ACTION_CANCEL")
+                Log.e("wjftc", "ChildView dispatchTouchEvent ACTION_CANCEL")
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val result = super.onTouchEvent(event)
+//        Log.d("wjftc","ChildView onTouchEvent:$result")
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("wjftc", "ChildView onTouchEvent ACTION_DOWN")
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.i("wjftc", "ParentLayout onTouchEvent ACTION_MOVE")
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.w("wjftc", "ChildView onTouchEvent ACTION_UP")
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.e("wjftc", "ChildView onTouchEvent ACTION_CANCEL")
             }
         }
         return result
     }
 
     override fun onClick(v: View?) {
-        Logger.d("ChildView onClick")
+        Log.w("wjftc", "ChildView onClick")
     }
 }
--- app/src/main/java/com/wjf/self_demo/widget/ParentLayout.kt
+++ app/src/main/java/com/wjf/self_demo/widget/ParentLayout.kt
@@ -2,10 +2,10 @@ package com.wjf.self_demo.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.orhanobut.logger.Logger
 
 class ParentLayout @JvmOverloads constructor(
     context: Context,
@@ -16,27 +16,64 @@ class ParentLayout @JvmOverloads constructor(
         setOnClickListener(this)
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val result = super.onTouchEvent(event)
-//        Logger.d("ParentLayout onTouchEvent:$result")
-        when (event?.action) {
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        when (ev?.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                Logger.d("ParentLayout ACTION_DOWN")
+                Log.d("wjftc", "ParentLayout dispatchTouchEvent ACTION_DOWN")
             }
             MotionEvent.ACTION_MOVE -> {
-//                Logger.d("ParentLayout ACTION_MOVE")
+                Log.i("wjftc", "ParentLayout dispatchTouchEvent ACTION_MOVE")
             }
             MotionEvent.ACTION_UP -> {
-                Logger.d("ParentLayout ACTION_UP")
+                Log.w("wjftc", "ParentLayout dispatchTouchEvent ACTION_UP")
             }
             MotionEvent.ACTION_CANCEL -> {
-                Logger.d("ParentLayout ACTION_CANCEL")
+                Log.e("wjftc", "ParentLayout dispatchTouchEvent ACTION_CANCEL")
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val result = super.onTouchEvent(event)
+//        Log.d("wjftc","ParentLayout onTouchEvent:$result")
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("wjftc", "ParentLayout onTouchEvent ACTION_DOWN $result")
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.i("wjftc", "ParentLayout onTouchEvent ACTION_MOVE $result")
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.w("wjftc", "ParentLayout onTouchEvent ACTION_UP $result")
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.e("wjftc", "ParentLayout onTouchEvent ACTION_CANCEL $result")
+            }
+        }
+        return result
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        val result = super.onInterceptTouchEvent(ev)
+        when (ev?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("wjftc", "ParentLayout onInterceptTouchEvent ACTION_DOWN  $result")
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.i("wjftc", "ParentLayout onInterceptTouchEvent ACTION_MOVE  $result")
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.w("wjftc", "ParentLayout onInterceptTouchEvent ACTION_UP  $result")
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.e("wjftc", "ParentLayout onInterceptTouchEvent ACTION_CANCEL  $result")
             }
         }
         return result
     }
 
     override fun onClick(v: View?) {
-        Logger.d("ParentLayout onClick")
+        Log.w("wjftc", "ParentLayout onClick")
     }
 }
